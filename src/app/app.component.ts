d50284eed3f28afb64147a1c86c0f25435399c30
Bruno Correia Caetano <bruno.correia.caetano@celfocus.com>
2018-11-09 11:45:06 +0000

    changed layout a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,13 @@ export class AppComponent implements OnInit {
   title = 'SideProject';
   isOpen = false;
 
+  options = [
+    {'name': 'home', 'path': 'home', 'icon': 'home'},
+    {'name': 'profile', 'path': 'profile', 'icon': 'person'},
+    {'name': 'career', 'path': 'career', 'icon': 'business_center'},
+    {'name': 'training', 'path': 'training', 'icon': 'school'}
+  ];
+
   constructor(private sideNavToggleService: SideNavToggleService) { }
 
 
