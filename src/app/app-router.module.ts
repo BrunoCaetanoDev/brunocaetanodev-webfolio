c2b48de33e27c7d9df9dfadc7ba0d9c944eba47f
Bruno Correia Caetano <bruno.correia.caetano@celfocus.com>
2018-12-05 14:25:31 +0000

    revert routing.

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -8,13 +8,12 @@ import {TrainingComponent} from './training/training.component';
 import {ContactInfoComponent} from './contact-info/contact-info.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent,  data: { animation: 'AnimatedPage'} },
-  { path: 'profile', component: ProfileComponent,  data: { animation: 'AnimatedPage'} },
-  { path: 'career', component: CareerComponent,  data: { animation: 'AnimatedPage'} },
-  { path: 'training', component: TrainingComponent,  data: { animation: 'AnimatedPage'} },
-  { path: 'contact-info', component: ContactInfoComponent, data: { animation: 'AnimatedPage'} }
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent,  data: {animation: 'AnimatedPage'} },
+  {path: 'profile', component: ProfileComponent,  data: {animation: 'AnimatedPage'} },
+  {path: 'career', component: CareerComponent,  data: {animation: 'AnimatedPage'} },
+  {path: 'training', component: TrainingComponent,  data: {animation: 'AnimatedPage'} },
+  {path: 'contact-info', component: ContactInfoComponent, data: {animation: 'AnimatedPage'} }
 ];
 
 @NgModule({
