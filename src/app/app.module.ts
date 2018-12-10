1ada1e8b26245496acc40d923af285bb59220684
Bruno Correia Caetano <bruno.correia.caetano@celfocus.com>
2018-12-10 16:14:34 +0000

    hammer js fix.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER  } from '@angular/core';
 
+import * as Hammer from 'hammerjs';
+
+import { HAMMER_GESTURE_CONFIG, HammerGestureConfig } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -23,6 +26,17 @@ import { TrainingComponent } from './training/training.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
 import {TranslateService} from './translate.service';
 import { TranslatePipe } from './translate.pipe';
+
+
+export class MyHammerConfig extends HammerGestureConfig {
+  buildHammer(element: HTMLElement) {
+    const mc = new Hammer(element, {
+      touchAction: 'pan-y'
+    });
+
+    return mc;
+  }
+}
 
 export function setupTranslateFactory(
   service: TranslateService): Function {
@@ -61,6 +75,10 @@ export function setupTranslateFactory(
       useFactory: setupTranslateFactory,
       deps: [ TranslateService ],
       multi: true
+    },
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig
     }
   ],
   bootstrap: [AppComponent]
