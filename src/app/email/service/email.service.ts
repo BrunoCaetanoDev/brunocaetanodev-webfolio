978b6b99aa6339a1f5a8781aec5489f09a8b3319
DESKTOP-BVONRVL\BRUNO <brunoacc92@gmail.com>
2018-12-13 22:40:26 +0000

    changed profile pic.

--- src/app/email/service/email.service.ts
+++ src/app/email/service/email.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Email} from '../model/email';
 
 import { environment } from '../../../environments/environment.prod';
--- src/app/profile/service/profile.service.ts
+++ src/app/profile/service/profile.service.ts
@@ -6,16 +6,18 @@ import { Profile} from '../model/profile';
 })
 export class ProfileService {
 
-  private readonly _profile : Profile;
+  private readonly _profile: Profile;
 
     constructor( ) {
       this._profile = new Profile();
-      this._profile.name = "Bruno";
-      this._profile.surname = "Caetano";
-      this._profile.image = "https://res.cloudinary.com/dnku44rpm/image/upload/v1544614965/profile.png";
-      this._profile.birthday = "15 November";
-      this._profile.birthplace= "Sintra";
-      this._profile.nationality= "Portuguese";
+      this._profile.name = 'Bruno';
+      this._profile.surname = 'Caetano';
+      // this._profile.image = "https://res.cloudinary.com/dnku44rpm/image/upload/v1544614965/profile.png";
+       this._profile.image = 'https://res.cloudinary.com/dnku44rpm/image/upload/v1544738703/profile.png';
+      // this._profile.image = 'https://res.cloudinary.com/dnku44rpm/image/upload/v1544740577/profile_custom.png';
+      this._profile.birthday = '15 November';
+      this._profile.birthplace = 'Sintra';
+      this._profile.nationality = 'Portuguese';
     }
 
   public get profile(): Profile {
