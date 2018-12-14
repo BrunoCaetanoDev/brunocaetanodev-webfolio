493b7e3624eb89b701543e333ab87bc667449fc1
Bruno Correia Caetano <bruno.correia.caetano@celfocus.com>
2018-12-14 11:27:16 +0000

    changed project name to devfolio.

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bruno-caetano-webfolio'));
+app.use(express.static(__dirname + '/dist/bruno-caetano-devfolio'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/bruno-caetano-webfolio/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/bruno-caetano-devfolio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
