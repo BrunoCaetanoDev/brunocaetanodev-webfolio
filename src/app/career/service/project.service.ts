e978f856cd1904ef0cc937386fa5b2ea196ccb96
Bruno Correia Caetano <bruno.correia.caetano@celfocus.com>
2019-04-23 15:19:19 +0100

    Modified career page.

--- src/app/career/service/project.service.ts
+++ src/app/career/service/project.service.ts
@@ -19,25 +19,24 @@ export class ProjectService {
 
     let project1  = new Project();
     project1.client = "Bee Engineering";
-    project1.department = "Bee Academy"
-    project1.startMonth = "SEPTEMBER"
+    project1.department = "Bee Academy";
+    project1.startMonth = "SEPTEMBER";
     project1.startYear = "2016";
-    project1.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac porttitor ligula, vitae luctus magna. Morbi egestas venenatis ornare. Donec maximus ullamcorper blandit. Nam venenatis tempus sem non tristique. Aenean blandit luctus pharetra. Nullam malesuada nisi vel lobortis egestas. Sed molestie libero at sem tincidunt efficitur. Morbi vel porttitor felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque interdum sodales felis. Cras tincidunt massa nec turpis viverra, in dignissim sapien aliquam."
+    project1.description = "BEE_SUMMARY";
 
     let project2  = new Project();
     project2.client = "TIMWE Group";
-    project2.department = "B2C"
-    project2.startMonth = "FEBRUARY"
+    project2.department = "B2C";
+    project2.startMonth = "FEBRUARY";
     project2.startYear = "2017";
-    project2.description = "Aliquam ultricies, urna sed gravida tristique, nulla quam efficitur tortor, dapibus gravida leo sem a justo. Quisque pharetra sapien quis condimentum congue. Vestibulum lobortis maximus dignissim. Etiam quam mauris, pellentesque ut urna sed, semper commodo mauris. In mollis id tortor ut bibendum. Mauris malesuada ipsum dui, ac semper dolor pretium a. Sed ultricies ex eu purus finibus aliquam. Vivamus sed lectus non sapien varius consectetur eget sit amet justo. Duis non consectetur diam. Nunc eget mi accumsan, aliquam neque eget, tempus lectus. Mauris vitae odio et arcu pharetra posuere. Suspendisse potenti."
+    project2.description = "TIMWE_SUMMARY";
 
     let project3  = new Project();
     project3.client = "Celfocus";
-    project3.department = "Consumer IoT"
-    project3.startMonth = "JULY"
+    project3.department = "Consumer IoT";
+    project3.startMonth = "JULY";
     project3.startYear = "2018";
-    project3.description = "Aliquam ultricies, urna sed gravida tristique, nulla quam efficitur tortor, dapibus gravida leo sem a justo. Quisque pharetra sapien quis condimentum congue. Vestibulum lobortis maximus dignissim. Etiam quam mauris, pellentesque ut urna sed, semper commodo mauris."
-
+    project3.description = "CELFOCUS_SUMMARY";
     this._projects.push(project3);
     this._projects.push(project2);
     this._projects.push(project1);
